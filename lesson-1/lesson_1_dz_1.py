c0ce8bcac9854939d78899e38851ec19a737fbb4
Максим Кузнецов <makuznet@imac.local>
2020-10-19 00:15:57 +0300

    added some comments

--- lesson-1/lesson_1_dz_1.py
+++ lesson-1/lesson_1_dz_1.py
@@ -1,11 +1,11 @@
 user_input = input('Введите число или текст: ') # любой ввод — это строка
 try:
-    value = int(user_input) # если вводить a = 5, Питон сразу преобразует тип переменной в int
+    value = int(user_input) # если с клавиатуры вводить a = 5, Питон сразу преобразует тип переменной в int
     print(value, '— целое число!')
     print('+, -, х, % — да! Конкатенация — нет!')
 except ValueError:
     try:
-        value = float(user_input) # если вводить b = 2.66, Питон сразу преобразует тип во float
+        value = float(user_input) # если c клавиатуры вводить b = 2.66, Питон сразу преобразует тип во float
         print(value, '— число с дробной частью?!')
         print('+, -, х, % — да! Конкатенация — нет!')
     except ValueError:

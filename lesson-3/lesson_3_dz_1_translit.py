215f4292c1e6044b8ca59ab458dcc5a16c65be94
makuznet <makuznet@gmail.com>
2020-11-08 12:04:05 +0300

    almost there

--- lesson-3/lesson_3_dz_1_translit.py
+++ lesson-3/lesson_3_dz_1_translit.py
@@ -1,8 +1,12 @@
 # программа принимает с клавиатуры слова, записанные кириллицей,
 # и печатает результат латиницей
-translit = {'е': '2', 'п': '3'}
-cyrillic = input('Введите текст для транслитерации: ')
-# [print(index, end='') for index in cyrillic]
+translit = {}
+cyrillic = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+latin = 'abvgdeejzijklmnoprstufhzcss_y_euaABVGDEEJZIJKLMNOPRSTUFHZCSS_Y_EUA'
+[translit.update({bukva: letter}) for bukva in cyrillic for letter in latin]
+print(translit)
+# translit = {'е': '2', 'п': '3'}
+# cyrillic = input('Введите текст для транслитерации: ')
+# [print(translit.get(index), end='') if index
+#  in translit else print(index, end='') for index in cyrillic]
 # print()
-[print(translit.get(index), end='') for index in cyrillic]
-print()

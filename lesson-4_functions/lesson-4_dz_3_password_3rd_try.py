5feee8a2c548550641e181081378e83933be5bea
makuznet <makuznet@gmail.com>
2020-11-17 23:18:59 +0300

    third attempt finished

---
+++ lesson-4_functions/lesson-4_dz_3_password_3rd_try.py
@@ -0,0 +1,37 @@
+# Используя генератор паролей создайте свой.
+#
+# passwd = ''
+# for x in range(12):
+#     passwd += random.choice(
+#         '123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')
+# print(passwd)
+#
+# Пароли, получаемые в результате, должны удовлетворять следующим условиям:
+#  - длина пароля - 15 символов
+#  - в пароле есть 3 заглавные буквы (любые, в любом месте пароля)
+#  - в пароле есть 4 цифры (любые, в любом месте)
+#  - оставшиеся символы пароля - строчные латинские буквы
+
+import random
+
+passwd = ''
+vocab = 'qwertyuiopasdfghjklzxcvbnm0123456789QWERTYUIOPASDFGHJKLZXCVBNM'
+
+while len(passwd) < 15:
+
+    candidate = random.choice(vocab)
+
+    lowcase = [symbol for symbol in passwd if symbol.islower()]
+    capital = [symbol for symbol in passwd if symbol.isupper()]
+    numbers = [symbol for symbol in passwd if symbol.isdigit()]
+
+    conditions = (
+        candidate.islower() and len(lowcase) < 8,
+        candidate.isupper() and len(capital) < 3,
+        candidate.isdigit() and len(numbers) < 4
+    )
+
+    if any(conditions):
+        passwd += candidate
+
+print('\n', passwd, '\n')

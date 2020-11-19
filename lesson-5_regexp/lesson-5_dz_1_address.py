d41476ce2f2af85a6a6149d0ecce2d14aeb8674b
makuznet <makuznet@gmail.com>
2020-11-19 19:45:53 +0300

    first try done

--- lesson-5_regexp/lesson-5_dz_1_address.py
+++ lesson-5_regexp/lesson-5_dz_1_address.py
@@ -1,1 +1,20 @@
-# \d{3,4}(\ [A-Z].+){2,}\n[A-Z].+\d{3,4}\n[A-Z].+\,\s[A-Z]{2}\ \d{5}
+import os
+import re
+path = os.path.dirname(os.path.abspath(__file__)) + '/addresses.txt'
+
+with open(path, 'r') as f:
+    for line in f:
+        try:
+            print(re.search(r'\d{3,4}(\ [A-Z].+){2,3}', line).group())
+        except:
+            result = None
+        try:
+            print(re.search(r'(Suite)\s\d{1,4}', line).group())
+        except:
+            result = None
+        try:
+            result3 = re.search(r'[A-Z].+\,\s[A-Z]{2}\ \d{5}', line).group()
+            print(result3, '\n')
+        except:
+            result = None
+f.close()
